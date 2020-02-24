847dad66c8fe0606bc9248231405745ca6f5ecd6
Jalia Evans <jalia@nete.com>
2020-02-24 12:16:25 -0500

    created list of json data

--- project-detail-page/src/Components/listings.js
+++ project-detail-page/src/Components/listings.js
@@ -1,3 +1,39 @@
 import React, {Component} from 'react';
-import List from "./../list.json";
+import ListData from "./../list.json";
 
+function displayData () {
+    console.log(ListData);
+}
+
+displayData();
+
+class Listings extends Component {
+    render() {
+        return (
+            <div>
+                <h1>List</h1>
+                <ul>
+                    {ListData.map((listDetail, index) => {
+                        return (
+                            <li>
+                                <h2>{listDetail.title}</h2>
+                                <p>{listDetail.name}</p>
+                                <p>{listDetail.appID}</p>
+                                <ul>
+                                    {listDetail.subItems.map((item, index) => {
+                                        return (<li>
+                                            {item.number}
+                                            {item.author}
+                                            </li>);
+                                    })}
+                                </ul>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Listings;
